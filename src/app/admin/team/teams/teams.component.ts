8037a54fc47114026877f158dbabe3be16735cb0
tusshan@yahoo.com <tusshan@yahoo.com>
2018-07-05 23:33:35 +0530

    Update validation

--- src/app/admin/team/teams/teams.component.ts
+++ src/app/admin/team/teams/teams.component.ts
@@ -35,7 +35,7 @@ export class TeamsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.rowSelection = "single";
+    this.rowSelection = 'single';
     this.http.get(Constants.apiUrl + '/users')
       .subscribe((data1: any) => {
         console.log('users', data1);
@@ -43,12 +43,12 @@ export class TeamsComponent implements OnInit {
       });
   }
   onSelectionChanged(event: any) {
-    var selectedRows = this.gridApi.getSelectedRows();
-    var selectedRowsString = "";
+    const selectedRows = this.gridApi.getSelectedRows();
+    let selectedRowsString = '';
     console.log(selectedRows);
     selectedRows.forEach(function (selectedRow, index) {
       if (index !== 0) {
-        selectedRowsString += ", ";
+        selectedRowsString += ', ';
       }
       selectedRowsString += selectedRow.model;
     });
@@ -60,27 +60,28 @@ export class TeamsComponent implements OnInit {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
     this.rowData = new Array();
-    let rows = new Array();
+    const rows = new Array();
 
     this.http
       .get(Constants.apiUrl + '/teams')
       .subscribe((data: any) => {
-        console.log("row teams",data)
+        console.log('row teams', data)
         data.forEach(element => {
-          let data1 = this.users.filter(a => a.id == element.CreatedBy)[0];
+          // tslint:disable-next-line:triple-equals
+          const data1 = this.users.filter(a => a.id == element.CreatedBy)[0];
           console.log('user', data1);
-          let team = new Team();
+          const team = new Team();
           team.id = element.id;
           team.TeamName = element.TeamName;
           if (element.TeamLead) {
-            let data2 = this.users.filter(a => a.id == element.TeamLead)[0];
+            const data2 = this.users.filter(a => a.id === element.TeamLead)[0];
             team.TeamLead = data2;
           }
           team.CreatedDate = element.CreatedDate;
           team.CreatedBy = data1;
           rows.push(team);
         });
-        console.log("teams",rows)
+        console.log('teams', rows)
         this.rowData = rows;
       });
   }
